6400d1702a78f98296fcb1203ced6638c6cf5cf4
Serge van den Oever <svdoever@gmail.com>
2020-07-31 01:03:42 +0200

    make react/vue/storybook  run under www - storybook still issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: {
     enabled: process.env.NODE_ENV !== 'development',
     mode: 'all',
-    content: ['src/**/*.tsx','src/**/*.html'],
+    content: [
+      'src/**/*.tsx','src/**/*.html',
+      'samples/sample-react/src/**/*.tsx',
+      'samples/sample-vue/src/**/*.vue'
+    ],
     // passed to PurgeCSS
     options: { 
       whitelist: []
